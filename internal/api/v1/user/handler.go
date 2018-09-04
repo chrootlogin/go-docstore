055371d83759d399f9be6193b2b6249e5445cc09
Simon Erhardt <hello@rootlogin.ch>
2018-09-04 22:08:51 +0200

    more tests

--- internal/api/v1/user/handler.go
+++ internal/api/v1/user/handler.go
@@ -1,10 +1,57 @@
 package user
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/chrootlogin/go-docstore/internal/common"
+	"github.com/chrootlogin/go-docstore/internal/store"
 )
 
+type apiResponse struct {
+	Username    string   `json:"username"`
+	Email       string   `json:"email"`
+	Permissions []string `json:"permissions"`
+}
+
 func GetUserHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{})
+	userName := c.Param("username")
+	if len(userName) <= 3 {
+		c.AbortWithStatusJSON(http.StatusBadRequest, common.ApiResponse{Message: common.WrongAPIUsageError})
+		return
+	}
+
+	// remove first character because it's always /
+	userName = trimLeftChar(userName)
+
+	user, err := store.Users().Get(userName)
+	if err != nil {
+		if err == store.ErrUserNotExist {
+			c.AbortWithStatusJSON(http.StatusNotFound, common.ApiResponse{Message: fmt.Sprintf("User not found: %s", userName)})
+			return
+		}
+
+		c.AbortWithStatusJSON(http.StatusInternalServerError, common.ApiResponse{Message: fmt.Sprintf("Can't get user list: %s", err.Error())})
+		return
+	}
+
+	resp := apiResponse{
+		Username:    user.Username,
+		Email:       user.Email,
+		Permissions: user.Permissions,
+	}
+
+	c.JSON(http.StatusOK, resp)
+}
+
+// https://stackoverflow.com/a/48798875
+func trimLeftChar(s string) string {
+	for i := range s {
+		if i > 0 {
+			return s[i:]
+		}
+	}
+	return s[:0]
 }
---
+++ internal/api/v1/user/handler_test.go
@@ -0,0 +1,77 @@
+package user
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"github.com/chrootlogin/go-docstore/internal/store"
+	"github.com/chrootlogin/go-docstore/internal/common"
+)
+
+// try existing user
+func TestGetUserHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	w := httptest.NewRecorder()
+
+	u := common.User{
+		Username: "test-user",
+		Email: "test@example.com",
+	}
+
+	err := store.Users().Add(u)
+	if assert.NoError(err) {
+		r := gin.Default()
+		r.GET("/user/*username", GetUserHandler)
+
+		req, _ := http.NewRequest("GET", "/user/" + u.Username, nil)
+		r.ServeHTTP(w, req)
+
+		if assert.Equal(w.Code, http.StatusOK) {
+			data, err := ioutil.ReadAll(w.Body)
+			if assert.NoError(err) {
+				var resp apiResponse
+				err = json.Unmarshal(data, &resp)
+				if assert.NoError(err) {
+					assert.Equal(u.Username, resp.Username)
+					assert.Equal(u.Email, resp.Email)
+				}
+			}
+		}
+	}
+}
+
+// try non existing user
+func TestGetUserHandler2(t *testing.T) {
+	assert := assert.New(t)
+
+	w := httptest.NewRecorder()
+
+	r := gin.Default()
+	r.GET("/user/*username", GetUserHandler)
+
+	req, _ := http.NewRequest("GET", "/user/not-existing", nil)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(http.StatusNotFound, w.Code)
+}
+
+// try with to short username
+func TestGetUserHandler3(t *testing.T) {
+	assert := assert.New(t)
+
+	w := httptest.NewRecorder()
+
+	r := gin.Default()
+	r.GET("/user/*username", GetUserHandler)
+
+	req, _ := http.NewRequest("GET", "/user/ab", nil)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(http.StatusBadRequest, w.Code)
+}
